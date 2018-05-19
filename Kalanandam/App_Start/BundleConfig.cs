c16d80a94057ed62dd26d90249070f120f8f3ac9
Subhadeep Dey <subhadeepdey@hotmail.com>
2018-05-19 00:42:54 -0600

    Added Slideshow

--- Kalanandam/App_Start/BundleConfig.cs
+++ Kalanandam/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Kalanandam
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/main.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +25,8 @@ namespace Kalanandam
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/main.css"));
         }
     }
 }
---
+++ Kalanandam/Scripts/main.js
@@ -0,0 +1,53 @@
+﻿$(function () {
+    var theImage = $('ul#ss li img');
+    var theWidth = theImage.width();
+    //wrap into mother div
+    $('ul#ss').wrap('<div id="mother" />');
+    //assign height width and overflow hidden to mother
+    $('#mother').css({
+        width: function () {
+            return theWidth;
+        },
+        height: function () {
+            return theImage.height();
+        },
+        position: 'relative',
+        overflow: 'hidden'
+    });
+    //get total of image sizes and set as width for ul 
+    var totalWidth = theImage.length * theWidth;
+    $('ul').css({
+        width: function () {
+            return totalWidth;
+        }
+    });
+
+    var ss_timer = setInterval(function () {
+        ss_next();
+    }, 3000);
+
+    function ss_next() {
+        var a = $(".active");
+        a.removeClass('active');
+
+        if (a.hasClass('last')) {
+            //last element -- loop
+            a.parent('ul').animate({
+                "margin-left": (0)
+            }, 1000);
+            a.siblings(":first").addClass('active');
+        } else {
+            a.parent('ul').animate({
+                "margin-left": (-(a.index() + 1) * theWidth)
+            }, 1000);
+            a.next().addClass('active');
+        }
+    }
+
+    // Cancel slideshow and move next manually on click
+    $('ul#ss li img').on('click', function () {
+        clearInterval(ss_timer);
+        ss_next();
+    });
+
+});
