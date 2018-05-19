4aa12ba2089f3edbd46afaa917b0cdbe204ba564
Subhadeep Dey <subhadeepdey@hotmail.com>
2018-05-19 16:19:05 -0600

    Added photo

--- Kalanandam/Scripts/main.js
+++ Kalanandam/Scripts/main.js
@@ -1,53 +1,10 @@
-﻿$(function () {
-    var theImage = $('ul#ss li img');
-    var theWidth = theImage.width();
-    //wrap into mother div
-    $('ul#ss').wrap('<div id="mother" />');
-    //assign height width and overflow hidden to mother
-    $('#mother').css({
-        width: function () {
-            return theWidth;
-        },
-        height: function () {
-            return theImage.height();
-        },
-        position: 'relative',
-        overflow: 'hidden'
-    });
-    //get total of image sizes and set as width for ul 
-    var totalWidth = theImage.length * theWidth;
-    $('ul').css({
-        width: function () {
-            return totalWidth;
-        }
-    });
+﻿$("#slideshow > div:gt(0)").hide();
 
-    var ss_timer = setInterval(function () {
-        ss_next();
-    }, 3000);
-
-    function ss_next() {
-        var a = $(".active");
-        a.removeClass('active');
-
-        if (a.hasClass('last')) {
-            //last element -- loop
-            a.parent('ul').animate({
-                "margin-left": (0)
-            }, 1000);
-            a.siblings(":first").addClass('active');
-        } else {
-            a.parent('ul').animate({
-                "margin-left": (-(a.index() + 1) * theWidth)
-            }, 1000);
-            a.next().addClass('active');
-        }
-    }
-
-    // Cancel slideshow and move next manually on click
-    $('ul#ss li img').on('click', function () {
-        clearInterval(ss_timer);
-        ss_next();
-    });
-
-});
+setInterval(function () {
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+}, 3000);
