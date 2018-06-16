7bb0edb56bb6c46b3320a96245dbc5b53ad3f6d6
Subhadeep Dey <subhadeepdey@hotmail.com>
2018-06-16 00:54:08 -0600

    Updated Content

--- Kalanandam/Controllers/HomeController.cs
+++ Kalanandam/Controllers/HomeController.cs
@@ -20,6 +20,14 @@ namespace Kalanandam.Controllers
             return View();
         }
 
+        public ActionResult Classes()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
